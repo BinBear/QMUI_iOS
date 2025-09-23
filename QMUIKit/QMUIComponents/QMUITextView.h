4f5f61ce52f140dadcb556ee87733eef18ee71b4
BinBear <vin404@outlook.com>
2025-09-23 15:20:41 +0800

    Update QMUITextView.h

--- QMUIKit/QMUIComponents/QMUITextView.h
+++ QMUIKit/QMUIComponents/QMUITextView.h
@@ -92,6 +92,11 @@
 @property(nonatomic, strong) IBInspectable UIColor *placeholderColor;
 
 /**
+ *  placeholder 文字大小
+ */
+@property(nonatomic, strong) IBInspectable UIFont *placeholderFont;
+
+/**
  *  placeholder 在默认位置上的偏移（默认位置会自动根据 textContainerInset、contentInset 来调整）
  */
 @property(nonatomic, assign) UIEdgeInsets placeholderMargins;
